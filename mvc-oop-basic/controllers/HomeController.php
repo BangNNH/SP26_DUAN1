df8b76d6f7ab748995c0116dace193fd80936f01
NguyenGiaBao <html310305@gmail.com>
2026-03-17 16:46:07 +0700

    vid6.3/loginClient

--- mvc-oop-basic/controllers/HomeController.php
+++ mvc-oop-basic/controllers/HomeController.php
@@ -3,9 +3,13 @@
 class HomeController
 {
     public $modelSanPham;
+    public $modelTaiKhoan;
+
+
     public function __construct()
     {
         $this->modelSanPham = new SanPham();
+        $this->modelTaiKhoan = new TaiKhoan();
     }
 
     public function home()
@@ -33,4 +37,36 @@ class HomeController
     //     $listProduct = $this->modelSanPham->getAllSanPham();
     //     require_once './views/listProduct.php';
     // }
+
+    public function formLogin(){
+        require_once __DIR__ . '/../views/auth/formLogin.php';
+        deleteSessionError();
+    }
+
+    public function postLogin(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            // lấy email và pass gửi lên từ form
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            // Xử lí kiểm tra thông tin đăng nhập
+            $user = $this->modelTaiKhoan->checkLogin($email, $password);
+
+            // Nếu trả về email (dạng string hợp lệ) là đăng nhập thành công
+            if (is_string($user) && filter_var($user, FILTER_VALIDATE_EMAIL)) {
+                // Lưu email vào session để dùng hiển thị
+                $_SESSION['user_client'] = $user;
+                header("Location: " . BASE_URL);
+                exit();
+            } else {
+                // Lỗi thì lưu lỗi vào session và xoá session user nếu có
+                unset($_SESSION['user_admin']);
+                $_SESSION['error'] = $user;
+                $_SESSION['flash'] = true;
+
+                header("Location: " . BASE_URL . '?act=login');
+                exit();
+            }
+        }
+    }
 }
--- mvc-oop-basic/index.php
+++ mvc-oop-basic/index.php
@@ -1,4 +1,5 @@
 <?php
+// session_start();
 
 // Require file Common
 require_once './commons/env.php'; // Khai báo biến môi trường
@@ -10,6 +11,7 @@ require_once './controllers/HomeController.php';
 // Require toàn bộ file Models
 require_once './models/Student.php';
 require_once './models/SanPham.php';
+require_once './models/TaiKhoan.php';
 
 // Route
 $act = $_GET['act'] ?? '/';
@@ -18,5 +20,11 @@ $act = $_GET['act'] ?? '/';
 match ($act) {
     '/' => (new HomeController)->home(),
     // 'danh-sach-san-pham' => (new HomeController)->danhSachSanPham(),
-    'chi-tiet-san-pham' => (new HomeController())->chiTietSanPham()
+    'chi-tiet-san-pham' => (new HomeController())->chiTietSanPham(),
+
+
+
+    // Auth
+    'login' => (new HomeController())->formLogin(),
+    'check-login' => (new HomeController())->postLogin(),
 };
